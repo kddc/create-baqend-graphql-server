7d57eea5860c030034057e1f08c279d77148919d
Julian Tiemann <jutiemann@gmail.com>
2017-10-20 10:21:00 +0200

    remove node fetch

--- test/schema/mutations.spec.js
+++ test/schema/mutations.spec.js
@@ -2,20 +2,8 @@
 import { db as baqend } from 'baqend/lib'
 import schema from '../schema.json'
 const host = 'http://127.0.0.1:8080/v1'
-import fetch from 'node-fetch'
-
-
-//const schema = [{"class":"/db/Test","superClass":"/db/Object","acl":{"schemaAdd":{"/db/Role/1":"allow"},"schemaReplace":{"/db/Role/1":"allow"},"schemaSubclass":{"/db/Role/1":"allow"}},"metadata":{},"fields":{"string":{"name":"string","type":"/db/String","order":0}}},{"class":"/db/User","superClass":"/db/Object","acl":{"schemaAdd":{"/db/Role/1":"allow"},"schemaReplace":{"/db/Role/1":"allow"},"schemaSubclass":{"/db/Role/1":"allow"}},"metadata":{},"fields":{"username":{"name":"username","type":"/db/String","order":0,"flags":["READONLY"]}}}]
 
 let db, tokenStorage
-
-// beforeAll(function() {
-//   tokenStorage = new baqend.util.TokenStorage();
-//   let emf = new baqend.EntityManagerFactory({host: host, tokenStorage: tokenStorage});
-//   return emf.createEntityManager(true).ready().then((em) => {
-//     return em.User.login('root', 'root');
-//   });
-// });
 
 beforeAll(async () => {
   let emf = new baqend.EntityManagerFactory({ host, tokenStorage })
@@ -33,16 +21,15 @@ beforeAll(async () => {
   })
 })
 
-// afterEach(() => {
-//   return Promise.all(Object.keys(schema).map(key => {
-//     return db.send(new db.message.TruncateBucket(schema[key]['class'].replace(/^\/db\//, ''))).catch(e => {})
-//   }))
-// })
+afterEach(() => {
+  return Promise.all(Object.keys(schema).map(key => {
+    return db.send(new db.message.TruncateBucket(schema[key]['class'].replace(/^\/db\//, ''))).catch(e => {})
+  }))
+})
 
 describe('it should parse field types correctly', () => {
   test(`test connection`, () => {
-    console.log(db.User.me)
-    expect(true).toBe(true)
+    expect(db.User.me.username).toBe('root')
     // expect(db.isReady).toBe(true)
     // expect(db.User.me.username).toBe('root')
     var bla = new db.Test({string: "bla"})
