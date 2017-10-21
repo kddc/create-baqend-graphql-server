f84e467f49210c2a823c95e2c02a89e0a84dff74
Julian Tiemann <jutiemann@gmail.com>
2017-10-22 00:47:36 +0200

    add field filter generator + tests

---
+++ src/schema/codegen/field/filterInputs.js
@@ -0,0 +1,30 @@
+import { codeBlock } from 'common-tags'
+
+/**
+ * Generates the field filter input definitions
+ *
+ * @param opts Some options to pass to the generator like which api should be used
+ * @param args Input arguments for generating the code
+ * @return The field input definitions
+ */
+const relay = ({ name, fieldType, elementType }) => {
+  const definitions = []
+  if (fieldType === 'collection') {
+    definitions.push(codeBlock`
+      ${name}: CollectionFilter
+    `)
+  } else if (fieldType === 'reference' || fieldType === 'embedded') {
+    definitions.push(codeBlock`
+      ${name}: ${elementType}Filter
+    `)
+  } else {
+    definitions.push(codeBlock`
+      ${name}: ${elementType}Filter
+    `)
+  }
+  return definitions
+}
+
+const generateFieldFilterInputDefinitions = (opts, args) => relay(args)
+
+export default generateFieldFilterInputDefinitions
---
+++ src/schema/codegen/field/filterInputs.spec.js
@@ -0,0 +1,63 @@
+import { codeBlock } from 'common-tags'
+import generateFieldFilterInputDefinitions from './filterInputs'
+
+describe('Generate field filter input definitions', () => {
+  test('it should generate a string filter input definition', () => {
+    const params = {
+      name: 'string',
+      fieldType: 'scalar',
+      elementType: 'String',
+    }
+    const expected = codeBlock`
+      string: StringFilter
+    `
+    const definition = generateFieldFilterInputDefinitions({}, params)
+    expect(definition.length).toBe(1)
+    expect(definition[0]).toEqual(expected)
+  })
+
+  test('it should generate a reference filter input definition', () => {
+    const params = {
+      name: 'reference',
+      fieldType: 'reference',
+      elementType: 'TestReference',
+    }
+    const expected = codeBlock`
+      reference: TestReferenceFilter
+    `
+    const definition = generateFieldFilterInputDefinitions({}, params)
+    expect(definition.length).toBe(1)
+    expect(definition[0]).toEqual(expected)
+  })
+
+  test('it should generate a embedded filter input definition', () => {
+    const params = {
+      name: 'embedded',
+      fieldType: 'embedded',
+      elementType: 'Embedded',
+    }
+    const expected = codeBlock`
+      embedded: EmbeddedFilter
+    `
+    const definition = generateFieldFilterInputDefinitions({}, params)
+    expect(definition.length).toBe(1)
+    expect(definition[0]).toEqual(expected)
+  })
+
+  test('it should generate a scalar collection filter input definition', () => {
+    const params = {
+      name: 'stringList',
+      fieldType: 'collection',
+      elementType: {
+        collectionType: 'List',
+        types: [{ fieldType: 'scalar', elementType: 'String' }],
+      },
+    }
+    const expected = codeBlock`
+      stringList: CollectionFilter
+    `
+    const definition = generateFieldFilterInputDefinitions({}, params)
+    expect(definition.length).toBe(1)
+    expect(definition[0]).toEqual(expected)
+  })
+})
